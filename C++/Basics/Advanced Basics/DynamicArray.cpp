90d0301833d247a8ff37faf0f9d725ae5a535ffe
Subhadip757 <140168312+Subhadip757@users.noreply.github.com>
2025-04-01 14:21:36 +0530

    refactor DynamicArray.cpp for improved readability and code style

--- C++/Basics/Advanced Basics/DynamicArray.cpp
+++ C++/Basics/Advanced Basics/DynamicArray.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 
 using namespace std;
-int getSum(int *arr, int n){
+int getSum(int *arr, int n)
+{
     int sum = 0;
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         sum += arr[i];
     }
     return sum;
 }
 
-int main(){
+int main()
+{
     // char ch = 'q';
     // cout<<sizeof(ch) << endl;
 
@@ -17,16 +20,29 @@ int main(){
     // cout << sizeof(c) << endl;
 
     int n;
-    cin>>n;
-    
-    //varirable size array
+    cin >> n;
+
+    // varirable size array
     int *arr = new int[n];
-    for(int i = 0; i<n; i++){
-        cin>>arr[i];
+    char *ch = new char[20];
+
+    for (int i = 0; i < 20; i++)
+    {
+        ch[i] = i + 'a';
     }
 
-    int ans = getSum(arr, n);
-    cout<<"answer is "<<ans << endl;
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
+
+    for (int i = 0; i < 20; i++)
+    {
+        cout << ch[i] << " ";
+    }
+
+    // int ans = getSum(arr, n);
+    // cout << "answer is " << ans << endl;
 
     return 0;
 }
